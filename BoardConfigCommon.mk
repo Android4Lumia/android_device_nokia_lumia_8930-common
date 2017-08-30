45871793c9629751baa29c5bd0672e7e91123f2a
Fehér Roland Ádám <feherneoh@gmail.com>
2017-08-30 02:36:08 -0700

    BoardConfigCommon.mk: Add missing CPU variant declaration

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -22,6 +22,7 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := krait
 
 # Product info
 BOARD_VENDOR := nokia
